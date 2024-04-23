97c23287ea0a2016abf3d182951f4704581eadce
vittor <vittor.pelicarto137@gmail.com>
2024-04-23 10:20:55 -0300

    add seed on attendees

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,49 @@
 import { prisma } from '../src/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+import dayjs from 'dayjs'
+
 
 async function seed() {
+  const eventId = '9e9bd979-9d10-4915-b339-3786b1634f33'
+
+  await prisma.event.deleteMany() 
+
   await prisma.event.create({
     data: {
-      id: 'f42ecc55-40f6-4f49-a3fd-4de496b452c7',
-      title: 'Unite Summit',
-      slug: 'unite-summit',
+      id: eventId,
+      title: 'React Summit',
+      slug: 'react-summit',
       details: 'Um evento p/ devs apaixonados(as) por código!',
       maximumAttendees: 120,
     }
   })
+
+  const attendeesToInsert: Prisma.AttendeeUncheckedCreateInput[] = []
+
+  for (let i = 0; i < 120; i++) {
+    attendeesToInsert.push({
+      id: 10000 + i,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      eventId,
+      createdAt: faker.date.recent({ days: 30, refDate: dayjs().subtract(8, "days").toDate() }),
+      checkIn: faker.helpers.arrayElement<Prisma.CheckInUncheckedCreateNestedOneWithoutAttendeeInput | undefined>([
+        undefined,
+        {
+          create: {
+            createdAt: faker.date.recent({ days: 7 }),
+          }
+        }
+      ])
+    })
+  }
+
+  await Promise.all(attendeesToInsert.map(data => {
+    return prisma.attendee.create({
+      data,
+    })
+  }))
 }
 
 seed().then(() => {
